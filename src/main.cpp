8b8bd71717796a1e0ff66cebcc017267881efc98
Fonzy Fermin <fonzy.fermin@gmail.com>
2024-04-06 17:39:35 +0800

    Initial window setup

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 #include <glad/glad.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+void process_input(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
 int main() {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -17,4 +25,29 @@ int main() {
     return -1;
   }
   glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD:" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    process_input(window);
+
+    // render
+
+    // swap buffers and call events
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
