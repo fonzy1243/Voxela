172f5f21e270ece682aa4c6d95ee620d2cd07003
Fonzy Fermin <fonzy.fermin@gmail.com>
2024-04-18 22:36:59 +0800

    Updated build files

--- build/CMakeFiles/voxela.dir/DependInfo.cmake
+++ build/CMakeFiles/voxela.dir/DependInfo.cmake
@@ -10,6 +10,9 @@ set(CMAKE_DEPENDS_LANGUAGES
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "C:/Users/Fonzy/Voxela/src/glad.c" "CMakeFiles/voxela.dir/src/glad.c.obj" "gcc" "CMakeFiles/voxela.dir/src/glad.c.obj.d"
   "C:/Users/Fonzy/Voxela/src/main.cpp" "CMakeFiles/voxela.dir/src/main.cpp.obj" "gcc" "CMakeFiles/voxela.dir/src/main.cpp.obj.d"
+  "C:/Users/Fonzy/Voxela/src/world/block.cpp" "CMakeFiles/voxela.dir/src/world/block.cpp.obj" "gcc" "CMakeFiles/voxela.dir/src/world/block.cpp.obj.d"
+  "C:/Users/Fonzy/Voxela/src/world/chunk.cpp" "CMakeFiles/voxela.dir/src/world/chunk.cpp.obj" "gcc" "CMakeFiles/voxela.dir/src/world/chunk.cpp.obj.d"
+  "C:/Users/Fonzy/Voxela/src/world/chunk_manager.cpp" "CMakeFiles/voxela.dir/src/world/chunk_manager.cpp.obj" "gcc" "CMakeFiles/voxela.dir/src/world/chunk_manager.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
