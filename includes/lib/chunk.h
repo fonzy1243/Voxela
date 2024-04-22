24749b74d47dfcd38492c78d9d3311528fd625e2
Fonzy Fermin <fonzy.fermin@gmail.com>
2024-04-23 01:01:19 +0800

    Chunks no longer hard-coded

--- includes/lib/chunk.h
+++ includes/lib/chunk.h
@@ -1,44 +1,37 @@
 #ifndef CHUNK_H
 #define CHUNK_H
 
+#include <array>
+#include <block.h>
 #include <camera.h>
 #include <glm/glm.hpp>
+#include <mesh.h>
 #include <shaders.h>
+#include <vector>
 
-static const float cube_vertices[] = {
-    -1.0, 1.0,  1.0,  // F top left
-    1.0,  1.0,  1.0,  // F top right
-    1.0,  -1.0, 1.0,  // F bottom right
-    -1.0, -1.0, 1.0,  // F bottom left
-    -1.0, 1.0,  -1.0, // B top left
-    1.0,  1.0,  -1.0, // B top right
-    1.0,  -1.0, -1.0, // B bottom right
-    -1.0, -1.0, -1.0  // B bottom left
-};
+constexpr int chunk_size = 32;
+constexpr int half_chunk_size = chunk_size / 2;
+constexpr int buff_size = chunk_size * chunk_size * chunk_size;
 
-static const int cube_indices[] = {
-    0, 3, 2, // Front
-    2, 1, 0, // Front
-    1, 5, 6, // Right
-    6, 2, 1, // Right
-    5, 4, 7, // Left
-    7, 6, 5, // Left
-    4, 7, 3, // Back
-    3, 0, 4, // Back
-    4, 5, 1, // Top
-    1, 0, 4, // Top
-    3, 2, 6, // Bottom
-    6, 7, 3, // Bottom
-};
+typedef std::array<std::array<std::array<Block, chunk_size>, chunk_size>,
+                   chunk_size>
+    BlockArray;
 
 class Chunk {
 public:
-  Chunk(glm::vec3 coordinates);
-  const void render(Camera camera);
+  Chunk(glm::ivec3 coordinates, BlockArray &blocks);
+  const void render();
+  void set_cube(int x, int y, int z, bool active);
 
 private:
   unsigned int vao, vbo, ebo, instance_vbo;
+  Mesh *voxel_mesh = nullptr;
+  unsigned int active_blocks = 0;
   glm::vec3 translations[32768];
+  BlockArray m_blocks;
+
+  void create_cube(std::vector<Vertex> &vertices,
+                   std::vector<unsigned int> &indices, int x, int y, int z);
 };
 
 #endif
